ff36eddb11985f2d94e9fc389fdd20adffd11eb4
Anuja <167699094+Anujashirahatti@users.noreply.github.com>
2024-04-26 19:35:39 +0530

    Add files via upload

---
+++ Bubble_sort.py
@@ -0,0 +1,18 @@
+lis=[4,2,1,5,6,12,8]
+n=len(lis)
+for i in range(n):
+    for j in range(0,n-i-1):
+        if lis[j] > lis[j+1]:
+            lis[j],lis[j+1]=lis[j+1],lis[j]
+print(lis)
+
+
+def bubble_sort(array):
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if lis[j] > lis[j+1]:
+                lis[j],lis[j+1]=lis[j+1],lis[j]
+lis=list(map(int,input("enter the list").split()))
+n=len(lis)
+bubble_sort(lis)
+print(lis)
